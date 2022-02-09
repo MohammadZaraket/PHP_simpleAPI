62415a9acaaa5ca2aa9fea2ea29d0bc181957ac5
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-02-09 13:24:36 +0200

    second api palindrome

--- palindrome.php
+++ palindrome.php
@@ -1,4 +1,21 @@
 <?php
 
+    $string = $_GET["input"];
+
+    $string = str_replace(' ', '', $string);
+    $string = strtolower($string);
+    $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string);
+
+
+    $reverse_string = strrev($string);
+
+    if ($string == $reverse_string) {
+        $status = ["status" => "YES Palindrome"];
+    } 
+    else {
+        $status = ["status" => "NO NOT Palindrome!"];
+    }
+
+echo json_encode($array);
 
 ?>
